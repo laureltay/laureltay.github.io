6f8328211cb02c5fddcfeaf4e0e703a6f01a663a
Laurel Tay <ltay3@u.rochester.edu>
2026-02-09 02:38:29 -0500

    new entry

--- src/posts.js
+++ src/posts.js
@@ -2,7 +2,7 @@ export const posts = [
   {
     slug: 'ReadMe',
     title: 'ReadMe',
-    date: '2025-01-03',
+    date: '2026-01-03',
     category: 'Thoughts',
     excerpt: '',
     featuredImage: '/images/hero.JPG',
@@ -10,15 +10,22 @@ export const posts = [
   {
     slug: 'beginagain',
     title: 'Begin Again',
-    date: '2025-01-05',
+    date: '2026-01-05',
     category: 'Thoughts',
     featuredImage: '/images/beginagain.JPG',
   },
   {
     slug: 'timetobreathe',
     title: 'A Break without a Break',
-    date: '2025-01-23',
+    date: '2026-01-23',
     category: 'Thoughts',
     featuredImage: '/images/breathe.JPG',
+  },
+  {
+    slug: 'worthit',
+    title: 'Tension of Opposites',
+    date: '2026-02-9',
+    category: 'Thoughts',
+    featuredImage: '/images/wintersunset.JPG',
   },
 ]
